2d0c8ee6c5f84a8d0de2a34342a7761185d1d016
DESKTOP-B8DETJS\PC <wmsauve@hotmail.com>
2025-10-01 12:06:04 +0900

    [ADD] Fixed up some code to using variables for commonly used parameters. [ADD] Animation fixes including animation for burger menu. [ADD] Fix some formatting for things that looked weird on mobile. [ADD] Font should work on samsung android devices.

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -7,12 +7,30 @@ import { RouterLink } from '@angular/router';
   templateUrl: './header.html',
   styleUrl: './header.scss'
 })
+
 export class Header {
   title = signal("William Sauvé");
 
   isMenuOpen = false;
 
-  toggleMenu() {
+  toggleMenu(event: Event) {
     this.isMenuOpen = !this.isMenuOpen;
+    const el = event.target as HTMLElement;
+    el.classList.remove("animatePulse"); 
+    void el.offsetWidth;              
+    el.classList.add("animatePulse");
+
+    const menu = document.getElementById("links") as HTMLElement;
+    if (menu) {
+      menu.classList.remove("slideInMenu"); 
+      menu.classList.remove("slideOutMenu");
+      void menu.offsetWidth;
+      if (this.isMenuOpen) {
+        menu.classList.add("slideInMenu");
+      }
+      else{
+        menu.classList.add("slideOutMenu");
+      }
+    }
   }
 }
